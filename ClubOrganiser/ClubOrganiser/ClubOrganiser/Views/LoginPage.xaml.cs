c93bd092cc3994f38ddc3c5fab95e4580619273e
Owen McDonnell <owen.mcdonnell@hpe.com>
2019-05-09 14:10:19 +0100

    Made a simple form that moves the app from a login page to a homepage. Does not validate data yet.

---
+++ ClubOrganiser/ClubOrganiser/ClubOrganiser/Views/LoginPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClubOrganiser.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage ()
+		{
+			InitializeComponent ();
+		}
+
+        public async void DoLogin(object sender, System.EventArgs e)
+        {
+            try
+            {                
+                await Navigation.PushModalAsync(new MainPage());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+	}
+}
