7ce46fe0a31ccc1a6755b8e6bddb94ea5a324ac7
Owen McDonnell <Owen McDonnell@DESKTOP-4LTL6CN>
2019-05-25 17:48:37 +0100

    UITEST Config, build and test project appear to be working.

---
+++ ClubOrganiser/ClubOrganiser/ClubOrganiser.UITest/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace ClubOrganiser.UITest
+{
+    public class Tests : AbstractSetup
+    {
+
+        public Tests(Platform platform) : base(platform)
+        {
+        }
+
+        [Test]
+        public void WelcomeTextIsDisplayed()
+        {
+            app.WaitForElement(c => c.Marked("Please Sign In!"));
+            app.Screenshot("Welcome screen.");
+
+            
+        }
+    }
+}
