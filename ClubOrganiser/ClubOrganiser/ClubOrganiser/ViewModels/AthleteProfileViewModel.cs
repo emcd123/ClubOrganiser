6f18f331610df5c45fa84af122422a8847ef137a
Owen McDonnell <Owen McDonnell@DESKTOP-4LTL6CN>
2019-06-16 14:05:28 +0100

    Added datamodels project to solution

--- ClubOrganiser/ClubOrganiser/ClubOrganiser/ViewModels/AthleteProfileViewModel.cs
+++ ClubOrganiser/ClubOrganiser/ClubOrganiser/ViewModels/AthleteProfileViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,7 +9,6 @@ namespace ClubOrganiser.ViewModels
     {
         public AthleteProfileViewModel()
         {
-
         }
     }
 }
