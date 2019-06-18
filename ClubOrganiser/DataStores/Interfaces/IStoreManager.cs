af1aaea66224b737577c25b78c75d4ca8c9ce3cf
Owen McDonnell <Owen McDonnell@DESKTOP-4LTL6CN>
2019-06-18 20:22:21 +0100

    Create a project to contain the real and mock datastores for the app. and initialise the base stores and store managers

---
+++ ClubOrganiser/DataStores/Interfaces/IStoreManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStores.Interfaces
+{
+    public interface IStoreManager
+    {
+        bool IsInitialized { get; }
+        Task InitializeAsync();
+
+        Task<bool> SyncAllAsync(bool syncUserSpecific);
+        Task DropEverythingAsync();
+    }
+}
